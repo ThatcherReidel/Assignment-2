ba84bd19d18b415167b109ae53befbfda93f8222
ThatcherReidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-04-20 09:48:00 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+diameter = float(input("Enter the diameter of the circle: "))
+radius = diameter / 2
+area = 3.14 * (radius ** 2)
+circumference = 2 * 3.14 * radius
+
+print("The area of the circle is:", area)
+print("The circumference of the circle is:", circumference)
